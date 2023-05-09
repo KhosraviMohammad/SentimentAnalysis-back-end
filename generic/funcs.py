de1722176e108015873a35c8ebc6725c3c6bcd05
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-10 01:29:26 +0430

    api_view_serializer_class decorator is created

--- generic/funcs.py
+++ generic/funcs.py
@@ -1,3 +1,5 @@
+import types
+
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
@@ -5,3 +7,34 @@ def analyze_sentiment(text):
     sentiment = SentimentIntensityAnalyzer()
     sent = sentiment.polarity_scores(text)
     return sent
+
+
+def api_view_serializer_class_getter(serializer_class_getter):
+    def _get_serializer_class(self):
+        return serializer_class_getter()
+
+    def _get_serializer(self, *args, **kwargs):
+        serializer_class = self.get_serializer_class()
+        return serializer_class(*args, **kwargs)
+
+    def decorator(func):
+        if not hasattr(func, 'cls'):
+            raise Exception(
+                '@api_view_serializer_class_getter can only decorate'
+                ' @api_view decorated functions')
+        apiview_cls = func.cls
+        apiview_cls.get_serializer_class = types.MethodType(
+            _get_serializer_class,
+            apiview_cls)
+        if not hasattr(apiview_cls, 'get_serializer'):
+            # In case get_serializer() method is missing.
+            apiview_cls.get_serializer = types.MethodType(
+                _get_serializer,
+                apiview_cls)
+        return func
+
+    return decorator
+
+
+def api_view_serializer_class(serializer_class):
+    return api_view_serializer_class_getter(lambda: serializer_class)
