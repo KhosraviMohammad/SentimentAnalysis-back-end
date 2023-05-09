f1bc4cbb1aa70ead90535242834934cc387802a6
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-10 00:58:33 +0430

    analyze_sentiment is created

--- generic/funcs.py
+++ generic/funcs.py
@@ -1,1 +1,7 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+
+def analyze_sentiment(text):
+    sentiment = SentimentIntensityAnalyzer()
+    sent = sentiment.polarity_scores(text)
+    return sent
