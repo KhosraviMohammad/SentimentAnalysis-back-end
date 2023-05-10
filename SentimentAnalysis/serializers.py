e6a8963ead9255bd40b5ff822f7fb4372da7a3c1
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-10 13:43:27 +0430

    max_length of TextSerializer.text is increased

--- SentimentAnalysis/serializers.py
+++ SentimentAnalysis/serializers.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class TextSerializer(serializers.Serializer):
-    text = serializers.CharField(max_length=200, required=False, allow_blank=True)
+    text = serializers.CharField(max_length=500, required=False, allow_blank=True)
